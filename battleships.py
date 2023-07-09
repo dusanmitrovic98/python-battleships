654673b68d3f41fd1d7f5bda7e22734169849620
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-09 22:50:31 +0200

    Update battleships.py

--- battleships.py
+++ battleships.py
@@ -52,3 +52,4 @@ def play_game():
     for turn in range(turns):
         print("Turn", turn + 1)
         guess_row = int(input("Guess Row (0-9): "))
+        guess_col = int(input("Guess Col (0-9): "))
